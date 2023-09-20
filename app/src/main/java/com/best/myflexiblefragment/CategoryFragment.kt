155210d17018cf4a9f208125eaee9d370713086d
abdul rohman <abdulrohmanunida@gmail.com>
2023-09-20 21:37:11 +0700

    add CategoryFragment.kt

--- app/src/main/java/com/best/myflexiblefragment/CategoryFragment.kt
+++ app/src/main/java/com/best/myflexiblefragment/CategoryFragment.kt
@@ -5,11 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-class CategoryFragment : Fragment() {
+class CategoryFragment : Fragment(), View.OnClickListener {
     private var param1: String? = null
     private var param2: String? = null
 
@@ -27,6 +28,18 @@ class CategoryFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_category, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val btnDetailCategory: Button = view.findViewById(R.id.btn_detail_category)
+        btnDetailCategory.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        if (v.id == R.id.btn_detail_category) {
+
+        }
     }
 
     companion object {
