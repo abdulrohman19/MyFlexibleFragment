8f9ec3ef39b34cd0df0868f947cc34ddcdc0fac7
abdul rohman <abdulrohmanunida@gmail.com>
2023-09-20 21:20:18 +0700

    delete comment layout fragment category

--- app/src/main/java/com/best/myflexiblefragment/CategoryFragment.kt
+++ app/src/main/java/com/best/myflexiblefragment/CategoryFragment.kt
@@ -10,7 +10,6 @@ private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
 class CategoryFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
@@ -31,15 +30,6 @@ class CategoryFragment : Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CategoryFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             CategoryFragment().apply {
